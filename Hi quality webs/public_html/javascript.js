ebd1b970b0aad159efc132e49853743151c531e0
I-can-have-a-long-name-if-I-want-to <ericxinzemiao@gmail.com>
2017-06-28 14:58:21 -0700

    stuff

--- Hi quality webs/public_html/javascript.js
+++ Hi quality webs/public_html/javascript.js
@@ -3,8 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*var i = 0;
 virus();
+
 function virus(){
-window.alert("Your System has been locked by the wanncrypt0r ransomware. Call Microsoft Certified technicians 1-844-310-3403")    ;
-virus();
+window.alert("Your System has been locked by the wanncrypt0r ransomware. Call Microsoft Certified technicians 1-844-310-3403")    ; 
+    if(i<10){
+        i++;
+        virus();
+    }
+}*/
+function  bvv(){
+    var x,y,z;
+    x = 15;
+    y = "5";
+    z = 6;
+    if (y === 5){
+        alert("troo");
+    }      else{
+        alert("fallse");
+    }
+}
+var listi = 
+        ["zero","one","two","three"];
+function listy(){
+    alert (listi[1]);
+}
+var object = {
+    name:"Unix_time", dob:"Jan 1, 1970", vehicle="wires, radio, and light"
+}
+function Unix_time(){
+    alert(Unix_time.name)
 }
